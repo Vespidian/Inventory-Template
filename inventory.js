23dacfe69223ddb18e8d2c0953044f372dfa31e3
Pimy <pimypp@gmail.com>
2017-07-15 16:54:00 -0400

    Inventory.

---
+++ inventory.js
@@ -0,0 +1,485 @@
+var inventory = [
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 0,
+        slotId: "cell0",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 1,
+        slotId: "cell1",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 2,
+        slotId: "cell2",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 3,
+        slotId: "cell3",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 4,
+        slotId: "cell4",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 5,
+        slotId: "cell5",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 6,
+        slotId: "cell6",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 7,
+        slotId: "cell7",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 8,
+        slotId: "cell8",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 9,
+        slotId: "cell9",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 10,
+        slotId: "cell10",
+        itemAmount: 0,
+        empty: true,
+      },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 11,
+          slotId: "cell11",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 12,
+          slotId: "cell12",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 13,
+          slotId: "cell13",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 14,
+          slotId: "cell14",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 15,
+          slotId: "cell15",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 16,
+          slotId: "cell16",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 17,
+          slotId: "cell17",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 18,
+          slotId: "cell18",
+          itemAmount: 0,
+          empty: true,
+        },
+        {
+          item: "empty",
+          itemNumber: 0,
+          slotNumber: 19,
+          slotId: "cell19",
+          itemAmount: 0,
+          empty: true,
+        },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 20,
+        slotId: "cell20",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 21,
+        slotId: "cell21",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 22,
+        slotId: "cell22",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 23,
+        slotId: "cell23",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 24,
+        slotId: "cell24",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 25,
+        slotId: "cell25",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 26,
+        slotId: "cell26",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 27,
+        slotId: "cell27",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 28,
+        slotId: "cell28",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 29,
+        slotId: "cell29",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 30,
+        slotId: "cell30",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 31,
+        slotId: "cell31",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 32,
+        slotId: "cell32",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 33,
+        slotId: "cell33",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 34,
+        slotId: "cell34",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 35,
+        slotId: "cell35",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 36,
+        slotId: "cell36",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 37,
+        slotId: "cell37",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 38,
+        slotId: "cell38",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 39,
+        slotId: "cell39",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 40,
+        slotId: "cell40",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 41,
+        slotId: "cell41",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 42,
+        slotId: "cell42",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 43,
+        slotId: "cell43",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 44,
+        slotId: "cell44",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 45,
+        slotId: "cell45",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 46,
+        slotId: "cell46",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 47,
+        slotId: "cell47",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 48,
+        slotId: "cell48",
+        itemAmount: 0,
+        empty: true,
+      },
+      {
+        item: "empty",
+        itemNumber: 0,
+        slotNumber: 49,
+        slotId: "cell49",
+        itemAmount: 0,
+        empty: true,
+      }
+    ]
+function inventoryAdd (itemN, cell, amount) {
+  var img = possibleItems[itemN].itemImage;
+  inventory[cell].item = possibleItems[itemN].itemName;
+  if (itemN >= 1) {
+    if (inventory[cell].itemAmount + amount >= 1) {
+    inventory[cell].empty = false;
+      if (inventory[cell].itemNumber == itemN) {
+        inventory[cell].itemAmount = inventory[cell].itemAmount + amount;
+        document.getElementById(inventory[cell].slotId).innerHTML = img;
+        document.getElementById(inventory[cell].slotId).innerHTML = document.getElementById(inventory[cell].slotId).innerHTML + inventory[cell].itemAmount;
+        inventory[cell].itemNumber = itemN;
+      } else if (inventory[cell].itemNumber !== itemN) {
+        inventory[cell].itemAmount = 0;
+        inventory[cell].itemAmount = inventory[cell].itemAmount + amount;
+        document.getElementById(inventory[cell].slotId).innerHTML = img;
+        document.getElementById(inventory[cell].slotId).innerHTML = document.getElementById(inventory[cell].slotId).innerHTML + inventory[cell].itemAmount;
+        inventory[cell].itemNumber = itemN;
+      }
+    }else if (inventory[cell].itemAmount + amount <= 0) {
+      document.getElementById(inventory[cell].slotId).innerHTML = " ";
+      inventory[cell].item = "empty";
+      inventory[cell].itemNumber = 0;
+      inventory[cell].itemAmount = 0;
+      inventory[cell].empty = true;
+    }
+  } else {
+    document.getElementById(inventory[cell].slotId).innerHTML = " ";
+    inventory[cell].item = "empty";
+    inventory[cell].itemNumber = 0;
+    inventory[cell].itemAmount = 0;
+    inventory[cell].empty = true;
+  }
+}
+//DO NOT EDIT BEFORE THIS LINE! (Unless you know what you're doing.)
+
+
+//ITEMS / FEEL FREE TO ADD AS MANY ITEMS AS YOU WANT. BUT ALWAYS KEEP THE EMPTY ITEM THERE AND FOLLOW THE SAME CONVENTIONS.
+var possibleItems = [
+  {
+    itemName: "empty",
+    itemImage: " ",
+    itemNumber: 0,
+  },
+  {
+    itemName: "Grass",
+    itemImage: "<img src='data/images/1.png'>",
+    itemNumber: 1,
+  },
+  {
+    itemName: "Dirt",
+    itemImage: "<img src='data/images/2.png'>",
+    itemNumber: 2,
+  },
+  {
+    itemName: "Sand",
+    itemImage: "<img src='data/images/3.png'>",
+    itemNumber: 3,
+  },
+  {
+    itemName: "Dirt",
+    itemImage: "<img src='data/images/4.png'>",
+    itemNumber: 4,
+  }
+]
+
+/*
+To add an item to the inventory, you have to use the following function:
+addToInv(itemnumber, cellnumber, itemamount);
+itemnumber being the item to be added to the inventory.
+cellnumber being the cell to add the item to.
+itemamount being the amount of the item to add.
+i.e:
+inventoryAdd(2, 23, 5);
+will add 5 piece of dirt to slot 23.
+to clear a slot use:
+inventoryAdd(0, cell, 0);
+cell being the slot you want to clear.
+*/
+//START YOUR WORK FROM HERE DOWN
+function setup (){
+//Add 7 grass to cell 5.
+  inventoryAdd(1, 5, 7);
+}
