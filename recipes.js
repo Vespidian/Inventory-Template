6d21d09a339a7c830c80f29cb5a8bc98677530e0
Pimy <pimypp@gmail.com>
2017-07-21 20:14:10 -0400

    Crafting!

---
+++ recipes.js
@@ -0,0 +1,13 @@
+function onCraft (item0, item1, item2, item3, itemN, amount) {
+  var img = possibleItems[itemN].itemImage;
+  if ((cInventory[0].item == item0) && (cInventory[1].item == item1)){
+    outSlot[0].item = possibleItems[itemN].item;
+    outSlot[0].itemNumber = possibleItems[itemN].itemNumber;
+    outSlot[0].itemAmount = amount;
+    outSlot[0].empty = false;
+    document.getElementById(outSlot[0].slotId).innerHTML = img;
+    document.getElementById(outSlot[0].slotId).innerHTML = document.getElementById(outSlot[0].slotId).innerHTML + "<p>" + outSlot[0].itemAmount + "</p>";
+
+    console.log("Dirt?");
+  }
+}
